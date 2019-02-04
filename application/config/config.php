e3a452f491f19b5ad0a94b71ad3a7b9512c5bc3e
Vihanga Bandara <vihanga123@gmail.com>
2019-02-04 15:40:09 +0530

    Updating config.php
    Removing base url address

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/AdvServCwk/';
+$config['base_url'] = 'http://localhost/{insert_your_folder_name}';
 
 /*
 |--------------------------------------------------------------------------
